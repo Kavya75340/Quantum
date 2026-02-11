18ebe52f3a2d37418209bb94370eeacdd87664e0
Kavya75340 <kvvarshney7534@gmail.com>
2026-02-11 12:33:11 +0530

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Footer } from "./component/layout/Footer";
 import { Navbar } from "./component/layout/Navbar";
 import AppRoutes from "./route/AppRoutes";
 import DevelopmentPopup from "./route/DevelopmentPopup";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
     return (
         <>
             <div className="min-h-screen flex flex-col">
+                <Analytics />
                 <DevelopmentPopup />
                 <Navbar />
                 <main className="flex-grow">
